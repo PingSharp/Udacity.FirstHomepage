48e862059ca19a38b456b2798f7f573e7a9cd1c0
dotPing <dotPing@online.de>
2018-04-16 19:40:46 +0200

    init commit

---
+++ js/PortfolioScript.js
@@ -0,0 +1,59 @@
+// JavaScript source code
+filterSelection("all")
+function filterSelection(c)
+{
+    var x, i;
+    x = document.getElementsByClassName("foliocolumn");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++)
+    {
+        RemoveClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+    }
+}
+function AddClass(element, name)
+{
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++)
+    {
+        if (arr1.indexOf(arr2[i]) == -1)
+        {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+function RemoveClass(element, name)
+{
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++)
+    {
+        while (arr1.indexOf(arr2[i]) > -1)
+        {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+function activchange(a) {
+    var portfoliofilter = document.getElementById("portfolio-filter");
+    var filter = portfoliofilter.getElementsByClassName("filter");
+
+    for (var i = 0; i < filter.length; i++) {
+        var classnames = filter[i].className.split(" ");
+        if (classnames.includes("gridactive"))
+        {
+           filter[i].className = filter[i].className.replace("gridactive", "");
+        }
+       
+            
+            
+        
+    }
+
+    var element = document.getElementById(a);
+    element.className += " " + "gridactive";
+}
